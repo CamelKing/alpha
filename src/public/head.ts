5a98b2b3adee067dbabae4f436acd7e466ab9e4b
Camel King <camelking@tataimages.com>
2017-04-17 01:10:31 +0800

    ref: head

--- src/public/head.ts
+++ src/public/head.ts
@@ -6,15 +6,17 @@
  * @since 0.0.1
  * @category Array
  *
+ * @refactor April 17, 2017
+ *
  * @export
  * @param {any[]} input
  * @returns {any[]}
  */
 
-import { nth } from './nth';
+// import { nth } from './nth';
 
 export function head(input: any[]): any {
-  return nth(input, 0);
-  // const [x] = input;
-  // return x;
+  // return nth(input, 0);
+  const [x] = input == null || !Array.isArray(input) ? [undefined] : input;
+  return x;
 }
